5d08dd3cf7e07c3c5d7a22887a17765990ca3645
Tejas Srikanth <tejas.c.srikanth.2020@outlook.com>
2021-01-16 15:55:55 -0500

    commit popup

--- interface1.py
+++ interface1.py
@@ -2,12 +2,22 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 from time import *
 from random import *
+from face_cropper import *
 
 root = Tk()
 screen = Canvas(root, width=600, height=600, background="white")
 screen.pack()
 
 currFile = ""
+
+def popupmsg(msg):
+    popup = Tk()
+    popup.wm_title("!")
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
 
 def getFile():
     filename = askopenfilename()
@@ -17,20 +27,26 @@ def getFile():
         currFile = False
 
     if not currFile:
-        badText = screen.create_text( 282.5, 350, text="Not valid file, please submit .jpg or .png", font="Times 12", fill="red")
+        badText = screen.create_text( 282.5, 250, text="Not valid file, please submit .jpg or .png", font="Times 12", fill="red")
         screen.update()
         sleep(0.5)
         screen.delete(badText)
         root.after(2, frontPage)
     else:
-        screen.create_text( 282.5, 350, text=filename, font="Times 12", fill="blue")
+        screen.create_text( 282.5, 250, text=filename, font="Times 12", fill="blue")
+        raw = r'{}'.format(filename)
+        try:
+            x = crop(raw)
+        except Exception as e:
+            popupmsg(e)
+            
         
         
 
 def frontPage():
     chooseFile = Button(root, text="Choose an image of your face", command=getFile)
     chooseFile.pack()
-    chooseFile.place(x=200, y=300)
+    chooseFile.place(x=200, y=200)
 
 root.after(0, frontPage)
 
