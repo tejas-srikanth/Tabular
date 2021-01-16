65e782c45a8d87aba9181aa88458586675fe3465
Rohan <rshetty449@gmail.com>
2021-01-16 15:38:01 -0500

    added face cropper and auto face encoder for new faces

--- face_encoder.py
+++ face_encoder.py
@@ -2,11 +2,12 @@ import face_recognition
 import pickle
 import os
 
-encodings = []
+def encode():
+    encodings = []
 
-for filename in os.listdir(os.path.join(os.path.dirname(__file__), "images/")):
-    img = face_recognition.load_image_file(os.path.join(os.path.dirname(__file__), "images/" + filename)) #will encode all images in this folder
-    encodings.append(face_recognition.face_encodings(img)[0])
+    for filename in os.listdir(os.path.join(os.path.dirname(__file__), "images/")):
+        img = face_recognition.load_image_file(os.path.join(os.path.dirname(__file__), "images/" + filename)) #will encode all images in this folder
+        encodings.append(face_recognition.face_encodings(img)[0])
 
-with open(os.path.join(os.path.dirname(__file__), "encoding.dat"), 'wb') as f:
-    pickle.dump(encodings, f)
+    with open(os.path.join(os.path.dirname(__file__), "encoding.dat"), 'wb') as f:
+        pickle.dump(encodings, f)
