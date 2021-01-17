07e1de0dd2b0d30fb26261f913496de781afcfa2
Matthews Ma <matthewsma21@gmail.com>
2021-01-17 00:24:07 -0500

    Clean up UI

--- interface1.py
+++ interface1.py
@@ -5,10 +5,22 @@ from random import *
 from face_cropper import *
 from pynput.keyboard import Listener, Key, Controller
 
-screen, currFile, root = "","",""
+
+WIDTH, HEIGHT = 400, 600
+FONT = ("Segoe UI Bold", 14)
+FONT_SECONDARY = ("Segoe UI", 14)
+FONT_TITLE = ("Segoe UI", 28)
+COLOUR_BACKGROUND = "#181818"
+COLOUR_PRIMARY = "white"
+COLOUR_SECONDARY = "#444"
+COLOUR_ACCENT = "#ecb100"
+
+
+screen, currFile, root = "", "", ""
 recorded = [Key.alt_l, Key.tab]
-recordedText= ""
+recordedText = ""
 keyboard = Controller()
+
 
 def main():
     global screen, currFile, root
@@ -20,17 +32,21 @@ def main():
     else:
         init_root()
 
+
 def on_closing():
     global root
     root.destroy()
     root = ""
-            
+
+
 def init_root():
     global screen, currFile, root
     root = Tk()
-    screen = Canvas(root, width=600, height=600, background="white")
+    root.title("Customize Tabular")
+    screen = Canvas(root, width=WIDTH, height=HEIGHT,
+                    background=COLOUR_BACKGROUND)
     screen.pack()
-    root.resizable(0,0)
+    root.resizable(0, 0)
     currFile = ""
     root.after(0, frontPage)
     root.protocol("WM_DELETE_WINDOW", on_closing)
@@ -41,11 +57,12 @@ def init_root():
 def popupmsg(msg):
     popup = Tk()
     popup.wm_title("!")
-    label = Label(popup, text=msg, font="Times 12")
+    label = Label(popup, text=msg, font=FONT)
     label.pack(side="top", fill="x", pady=10)
-    B1 = Button(popup, text="Okay", command = popup.destroy)
+    B1 = Button(popup, text="Okay", command=popup.destroy)
     B1.pack()
     popup.mainloop()
+
 
 def getFile():
     global screen, currFile, root
@@ -56,26 +73,30 @@ def getFile():
         currFile = False
 
     if not currFile:
-        badText = screen.create_text( 282.5, 250, text="Not valid file, please submit .jpg or .png", font="Times 12", fill="red")
+        badText = screen.create_text(
+        40, 250, text="Not a valid file", font=FONT_SECONDARY, fill="red", anchor=NW,
+        width=320)
         screen.update()
-        sleep(0.5)
+        sleep(2)
         screen.delete(badText)
-        root.after(2, frontPage)
+        # root.after(2, frontPage)
     else:
         try:
             raw = r'{}'.format(filename)
             x = crop(raw)
-            screen.create_text( 282.5, 250, text=filename, font="Times 12", fill="blue")
+            screen.create_text(
+                40, 250, text=filename, font=FONT_SECONDARY, fill=COLOUR_ACCENT, anchor=NW,
+                width=320)
         except Exception as e:
             popupmsg(e)
-              
+
+
 def log_keystroke(key):
 
     if key == Key.esc:
         return False
 
     recorded.append(key)
-
 
 
 def recordKeybind():
@@ -90,15 +111,27 @@ def recordKeybind():
             temp.append(str(recorded[x])[4:])
         else:
             temp.append(str(recorded[x]))
-    recordedText = screen.create_text( 282.5, 450, text=temp, font="Times 12", fill="blue")
+    recordedText = screen.create_text(
+        40, 470, text=temp, font=FONT_SECONDARY, fill=COLOUR_ACCENT, anchor=NW,
+        width=320)
 
 
 def frontPage():
     global screen, currFile, root, recordedText
-    chooseFile = Button(root, text="Choose an image of your face", command=getFile)
+    titleText = screen.create_text(
+        40, 50, text="Customize", font=FONT_TITLE, fill=COLOUR_PRIMARY, anchor=NW)
+
+    chooseFile = Button(root, text="CHOOSE AN IMAGE",
+                        command=getFile, bg=COLOUR_SECONDARY, fg=COLOUR_PRIMARY, bd=0, font=FONT,
+                        width=30, pady=15)
     chooseFile.pack()
-    chooseFile.place(x=200, y=200)
-    recordButton = Button(root, text="Set Keybind (press esc to stop recording)", command=recordKeybind)
+    chooseFile.place(x=WIDTH/2, y=200, anchor=CENTER)
+
+    recordButton = Button(root, text="SET KEYBIND",
+                          command=recordKeybind, bg=COLOUR_SECONDARY, fg=COLOUR_PRIMARY, bd=0, font=FONT,
+                          width=30, pady=15)
     recordButton.pack()
-    recordButton.place(x=175, y=400)
-    recordedText = screen.create_text( 282.5, 450, text=recorded, font="Times 12", fill="blue")
+    recordButton.place(x=WIDTH/2, y=400, anchor=CENTER)
+    recordedText = screen.create_text(
+        40, 470, text="alt_l tab", font=FONT_SECONDARY, fill=COLOUR_ACCENT, anchor=NW,
+        width=320)
